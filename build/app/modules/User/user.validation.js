2273daac4195f42a6bfee059a8f2a28900977efd
Habibur Rahman <va.habibur@gmail.com>
2025-08-22 14:00:23 +0600

    feat: add build

--- build/app/modules/User/user.validation.js
+++ build/app/modules/User/user.validation.js
@@ -3,10 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.signUpValidationSchema = void 0;
 const zod_1 = require("zod");
 exports.signUpValidationSchema = zod_1.z.object({
-    firstName: zod_1.z.string().nonempty('First name is required'),
-    lastName: zod_1.z.string().nonempty('Last name is required'),
+    name: zod_1.z.string().nonempty('name is required'),
     email: zod_1.z.string().email('Invalid email address'),
     password: zod_1.z.string().min(6, 'Password must be at least 6 characters'),
-    otpCode: zod_1.z.string().nonempty('OTP code is required'),
-    otpExpiresAt: zod_1.z.instanceof(Date).optional(),
 });
